25204b2f6252efd404ff6625546f605fd9446316
JoaoPedroMassmann <110121610+JoaoPedroMassmann@users.noreply.github.com>
2026-08-31 19:24:58 -0300

    fix: adicionar interface ConfiguracoesRepository corretamente

--- app/src/main/java/br/edu/utfpr/roadifylogger/data/repository/ConfiguracoesRepository.kt
+++ app/src/main/java/br/edu/utfpr/roadifylogger/data/repository/ConfiguracoesRepository.kt
@@ -1,3 +1,9 @@
 package br.edu.utfpr.roadifylogger.data.repository
 
 import br.edu.utfpr.roadifylogger.data.model.Configuracoes
+import kotlinx.coroutines.flow.StateFlow
+
+interface ConfiguracoesRepository {
+    val configuracoes: StateFlow<Configuracoes>
+    fun salvarConfiguracoes(configuracoes: Configuracoes)
+}
